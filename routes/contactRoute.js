8e6ee220610f56a1dac2988e7ee3e2b5125b156b
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-05-01 15:51:15 -0400

    Add captcha verification to contact creation route

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { createContact, getContacts } = require('../controllers/contactControl');
+const verifyCaptcha = require('../utils/verifyCaptcha');
 
-router.post('/', createContact);
+router.post('/', verifyCaptcha, createContact);
 router.get('/', getContacts);
 
 module.exports = router;
